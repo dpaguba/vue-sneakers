595b2adfd77b3ad39e9a4ff4943586eab135200d
Dmytro Pahuba <dpaguba@gmail.com>
2023-12-15 14:27:02 +0100

    feat: import card component and created title section

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <script setup>
 import Header from './components/Header.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header></Header>
 
-    <div>
-      <div
-        class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2 hover:shadow-xl transition"
-      >
-        <img src="/like-2.svg" alt="Like 1" class="absolute top-8 left-8" />
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
-        <p class="mt-2">Nike Blazer Mid Suede</p>
-
-        <div class="flex justify-between mt-5">
-          <div class="flex flex-col">
-            <span class="text-slate-400">Price:</span>
-            <b> 120$ </b>
-          </div>
-          <img src="/plus.svg" alt="Plus" />
-        </div>
+    <div class="p-10">
+      <h2 class="text-3xl font-bold">All Sneakers</h2>
+      <div>
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   </div>
