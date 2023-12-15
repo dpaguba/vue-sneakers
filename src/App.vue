62cd858855d16f6ecb5ddffc66801f0500a68bf4
Dmytro Pahuba <dpaguba@gmail.com>
2023-12-15 13:56:29 +0100

    feat: create header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div class="bg-white w-4/5 h-screen m-auto rounded-xl shadow-xl mt-14">
+    <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="logo" class="w-10" />
+        <div>
+          <h2 class="text-xl font-bold uppercase">Sneakers</h2>
+          <p class="text-slate-400">The store of the best sneakers</p>
+        </div>
+      </div>
+
+      <ul class="flex items-center gap-10">
+        <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/cart.svg" alt="Cart" />
+          <b>120 $</b>
+        </li>
+
+        <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/heart.svg" alt="Cart" />
+          <span>Bookmarks</span>
+        </li>
+
+        <li class="flex items-center gap-3 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/profile.svg" alt="Cart" />
+          <span>Profile</span>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
 
 <style scoped></style>
