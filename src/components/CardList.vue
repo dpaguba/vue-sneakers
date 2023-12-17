1c18fee539ab9c61d5ea04b357de83c3fbb267d0
Dmytro Pahuba <dpaguba@gmail.com>
2023-12-17 14:29:00 +0100

    feat: create example card

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,11 +1,18 @@
 <script setup>
 import Card from './Card.vue'
+
+const onClickAdd = () => {}
+const onClickFavorite = () => {}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card title="Nike Blazer Mid Suede" :price="120" />
-    <Card />
-    <Card />
+    <Card
+      title="Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :price="120"
+      :on-click-favorite="onClickAdd"
+      :on-click-add="onClickFavorite"
+    />
   </div>
 </template>
